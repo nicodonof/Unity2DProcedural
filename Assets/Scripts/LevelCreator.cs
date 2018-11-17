a6363862c5de6b7da8f3fbe77d90082f655ace1b
Nicolas D'Onofrio <nicodonofrio@gmail.com>
2018-11-17 20:22:02 -0300

    Correct grid script

--- Assets/Scripts/LevelCreator.cs
+++ Assets/Scripts/LevelCreator.cs
@@ -9,9 +9,22 @@ public class LevelCreator : MonoBehaviour {
 	public GameObject block;
 	// Use this for initialization
 	void Start () {
+		float widthGrid = Mathf.Round(Camera.main.pixelWidth/grid_row);
+		float widthCube = block.GetComponent<BoxCollider2D>().size.x;
+		print(widthCube);
 		for(int i = 0; i<grid_col;i++){
-			// Instantiate(block);
-		}
+			GameObject aux = Instantiate(block);
+			aux.transform.localScale = new Vector3(1,1,1);
+			// aux.transform.position = Camera.main.ScreenToWorldPoint(new Vector3(widthCube / 2  + widthCube /2 * i, widthCube / 2, 11));
+			aux.transform.position = Camera.main.ScreenToWorldPoint(new Vector3(0,0,11));
+			aux.transform.position = new Vector3(
+				aux.transform.position.x + (widthCube * i) + (widthCube / 2) ,
+				aux.transform.position.y + (widthCube / 2), 
+				aux.transform.position.z
+				 );
+
+
+    }
 	}
 	
 	// Update is called once per frame
