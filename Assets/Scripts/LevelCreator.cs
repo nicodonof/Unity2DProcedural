b139e959766d02f5753f531ef1ef2f16b61d6097
Nicolas D'Onofrio <nicodonofrio@gmail.com>
2018-11-17 15:27:36 -0300

    VsCode support + basics for level creation

---
+++ Assets/Scripts/LevelCreator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelCreator : MonoBehaviour {
+	public int grid_row = 8;
+	public int grid_col = 16;
+
+	public GameObject block;
+	// Use this for initialization
+	void Start () {
+		for(int i = 0; i<grid_col;i++){
+			// Instantiate(block);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
