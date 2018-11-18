ed0df506ae7151c84acba0da06d6fba40eec7e50
Nicolas D'Onofrio <nicodonofrio@gmail.com>
2018-11-18 19:21:08 -0300

    Better movement and jumping animations

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -15,25 +15,36 @@ public class PlayerScript : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
 		float x = rigid.velocity.x;
     float y = rigid.velocity.y;
 		if(Input.GetKey("right")){
-			rigid.velocity = 
-				new Vector2(Mathf.Clamp(x + Time.deltaTime + 1, -7,7),
-				y
-				);
+			rigid.velocity = new Vector2(
+					Mathf.Clamp(x + Time.deltaTime + 1, -7,7),
+					y
+			);
 		} else if(Input.GetKey("left")){
-			rigid.velocity = 
-				new Vector2(Mathf.Clamp(x - Time.deltaTime - 1, -7,7),
+			rigid.velocity = new Vector2(
+				Mathf.Clamp(x - Time.deltaTime - 1, -7,7),
 				y
-				);
+			);
 		}
-		if(Input.GetButtonDown("Jump") && rigid.velocity.y == 0){
-			rigid.AddForce(new Vector2(0,400));
+		if(Input.GetButtonDown("Jump") && rigid.velocity.y <= 0.1){
+			rigid.AddForce(new Vector2(0,700));
+			animator.SetBool("Jump", true);
 		}
+		// print();
+		
 
 		animator.SetFloat("Speed", rigid.velocity.x);
 		animator.SetFloat("SpeedY", rigid.velocity.y);
+	}
+
+	void OnCollisionEnter2D(Collision2D coll){
+// RaycastHit2D hit = Physics2D.Raycast(new Vector2(coll.bounds.min.x, coll.bounds.min.y), -Vector2.up, 0.01f);
+        if (coll.gameObject.tag == "Floor"){
+            print("Cackinas");
+            animator.SetBool("Jump", false);
+        }
 	}
 }
