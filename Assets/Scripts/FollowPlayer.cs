0a81bb5c5a592e04a70b59b731a5ef0b2a27acae
Nicolas D'Onofrio <nicodonofrio@gmail.com>
2018-11-17 21:31:52 -0300

    Decent movement + camera

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour {
+
+	private GameObject player;
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3(
+			(player.transform.position.x>0)?player.transform.position.x:0,
+			0,
+			-10
+		);
+	}
+}
---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    float x = GetComponent<Rigidbody2D>().velocity.x;
+    float y = GetComponent<Rigidbody2D>().velocity.y;
+		GetComponent<Rigidbody2D>().velocity = 
+			new Vector2(Mathf.Clamp(Input.GetAxis("Horizontal") + x, -5,5),y);
+		
+	}
+}
