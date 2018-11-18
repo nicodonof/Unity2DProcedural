1d1e0d140e62000c31c3690d0e44eeb2bf2cea94
Nicolas D'Onofrio <nicodonofrio@gmail.com>
2018-11-18 00:01:37 -0300

    Jumping mechonicks

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -3,18 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerScript : MonoBehaviour {
-
+	Rigidbody2D rigid;
+  BoxCollider2D coll;
 	// Use this for initialization
 	void Start () {
+    rigid = GetComponent<Rigidbody2D>();
+    coll = GetComponent<BoxCollider2D>();
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-    float x = GetComponent<Rigidbody2D>().velocity.x;
-    float y = GetComponent<Rigidbody2D>().velocity.y;
-		GetComponent<Rigidbody2D>().velocity = 
-			new Vector2(Mathf.Clamp(Input.GetAxis("Horizontal") + x, -5,5),y);
-		
+		float x = rigid.velocity.x;
+    float y = rigid.velocity.y;
+		rigid.velocity = 
+			new Vector2(Mathf.Clamp(Input.GetAxis("Horizontal") + x + Time.deltaTime, -7,7),
+			y
+			);
+		if(Input.GetButtonDown("Jump") && rigid.velocity.y == 0){
+			rigid.AddForce(new Vector2(0,400));
+		}
 	}
 }
