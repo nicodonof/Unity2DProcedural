d2c14acfe5312ed11bfb22406c1ccbbf85bca510
Nicolas D'Onofrio <nicodonofrio@gmail.com>
2018-11-18 15:57:22 -0300

    Better tinier level pieces

--- Assets/Scripts/LevelCreator.cs
+++ Assets/Scripts/LevelCreator.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class LevelCreator : MonoBehaviour {
-	public int chunkSize = 10;
+	public int chunkSize = 50;
 	float widthCube;
 	int chunkIndex;
 	Vector3 mapBeggining;
@@ -15,17 +15,17 @@ public class LevelCreator : MonoBehaviour {
 		// float widthGrid = Mathf.Round(Camera.main.pixelWidth/grid_row);
 		chunkIndex = 0;
 		mapBeggining = Camera.main.ScreenToWorldPoint(new Vector3(0, 0, 11));
-		widthCube = block.GetComponent<BoxCollider2D>().size.x;
+		// block.transform.localScale = new Vector3(0.5f,0.5f,1);
+		widthCube = block.GetComponent<BoxCollider2D>().size.x / 2;
 		chunks = new Queue<GameObject[]>();
 		CreateChunk(true);
-    CreateChunk(true);
-    CreateChunk(true);
+		CreateChunk(true);
+		CreateChunk(true);
 		player = GameObject.FindGameObjectWithTag("Player");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		// print(player.transform.position.x);
 		if(player.transform.position.x > ((chunkIndex-2) * widthCube * chunkSize)){
 			CreateChunk(false);
 		}
@@ -37,23 +37,20 @@ public class LevelCreator : MonoBehaviour {
 			Dechucker();
 		}
 
-		//Por ahora esto es un array feo
-		// chunks[chunkIndex] = new GameObject[chunkSize];
-
 		//Create the floor
-    GameObject[] auxChunk = new GameObject[chunkSize];
+    	GameObject[] auxChunk = new GameObject[chunkSize];
 		for (int i = 0; i < chunkSize; i++){
-      GameObject aux = Instantiate(block);
-      aux.transform.localScale = new Vector3(1, 1, 1);
-      // aux.transform.position = Camera.main.ScreenToWorldPoint(new Vector3(widthCube / 2  + widthCube /2 * i, widthCube / 2, 11));
-      aux.transform.position = mapBeggining;
-      aux.transform.position = new Vector3(
-        aux.transform.position.x + (widthCube / 2) + (widthCube * i) + (chunkSize * chunkIndex * widthCube),
-        aux.transform.position.y + (widthCube / 2),
-        aux.transform.position.z
-         );
+			GameObject aux = Instantiate(block);
+			// aux.transform.localScale = new Vector3(1, 1, 1);
+			// aux.transform.position = Camera.main.ScreenToWorldPoint(new Vector3(widthCube / 2  + widthCube /2 * i, widthCube / 2, 11));
+			aux.transform.position = mapBeggining;
+			aux.transform.position = new Vector3(
+			aux.transform.position.x + (widthCube / 2) + (widthCube * i) + (chunkSize * chunkIndex * widthCube),
+			aux.transform.position.y + (widthCube / 2),
+			aux.transform.position.z
+			);
 			auxChunk[i] = aux;
-    }
+  		}	
 		chunks.Enqueue(auxChunk);
 
 		chunkIndex += 1;
