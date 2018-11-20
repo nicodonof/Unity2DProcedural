5f719f3bb903cb954eecd8f00ee71c185d16722d
matuteale <mbuscagl@itba.edu.ar>
2018-11-20 20:16:57 -0300

    Add changing background by highscore

--- Assets/Scripts/BackgroundManager.cs
+++ Assets/Scripts/BackgroundManager.cs
@@ -16,8 +16,12 @@ public class BackgroundManager : MonoBehaviour {
 	private GameObject[] currFloor;
 	private GameObject[] currClouds;
 
+	private PlayerScript playerScript;
+
+	private int levelIndex = 0;
+
 	void Start () {
-		currBkg = Instantiate(background[0]);
+		currBkg = Instantiate(background[levelIndex]);
 		currBkg.transform.SetParent(transform);
 		currBkg.transform.localScale = new Vector3(7,5,1);
 		mainC = Camera.main;
@@ -26,20 +30,39 @@ public class BackgroundManager : MonoBehaviour {
 		temporaryCackinas = 0;
 		currFloor = new GameObject[2];
 		currClouds = new GameObject[2];
-		currFloor[0] = Instantiate(floorDecor[0]);
+		currFloor[0] = Instantiate(floorDecor[levelIndex]);
 		currFloor[0].transform.SetParent(transform);
-		currFloor[1] = Instantiate(floorDecor[0]);
+		currFloor[1] = Instantiate(floorDecor[levelIndex]);
 		currFloor[1].transform.SetParent(transform);
-		currClouds[0] = Instantiate(clouds[0]);
+		currClouds[0] = Instantiate(clouds[levelIndex]);
 		currClouds[0].transform.SetParent(transform);
-		currClouds[1] = Instantiate(clouds[0]);
+		currClouds[1] = Instantiate(clouds[levelIndex]);
 		currClouds[1].transform.SetParent(transform);
+		playerScript = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerScript>();
 
 		startParalax();
 	}
 
+	private void setNewBackground(){
+		currBkg.GetComponent<SpriteRenderer>().sprite = background[levelIndex].GetComponent<SpriteRenderer>().sprite;
+		currFloor[0].GetComponent<SpriteRenderer>().sprite = floorDecor[levelIndex].GetComponent<SpriteRenderer>().sprite;
+		currFloor[1].GetComponent<SpriteRenderer>().sprite = floorDecor[levelIndex].GetComponent<SpriteRenderer>().sprite;
+		currClouds[0].GetComponent<SpriteRenderer>().sprite = clouds[levelIndex].GetComponent<SpriteRenderer>().sprite;
+		currClouds[1].GetComponent<SpriteRenderer>().sprite = clouds[levelIndex].GetComponent<SpriteRenderer>().sprite;
+	}
+
 	// Update is called once per frame
 	void FixedUpdate () {
+		if(playerScript.highscore >= 100 && levelIndex == 0){
+			levelIndex = 1;
+			setNewBackground();
+		} else if(playerScript.highscore >= 200 && levelIndex == 1){
+			levelIndex = 2;
+			setNewBackground();
+		} else if(playerScript.highscore >= 300 && levelIndex == 2){
+			levelIndex = 3;
+			setNewBackground();
+		}
 		if(currFloor[0].transform.position.x < -15){
             currFloor[0].transform.position = mapBeggining + new Vector3(Random.Range(20f, 30f), 2, -1);
         }
